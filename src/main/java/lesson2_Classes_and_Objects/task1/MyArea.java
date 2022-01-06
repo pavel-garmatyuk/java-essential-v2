acd3dc96ff702a35272f2c36908f0e9abe3da2c0
Pavel Garmatyuk <m-krum@yandex.ru>
2022-01-06 18:08:16 +0300

    Java Essential HW2: task1 Classes is done

---
+++ src/main/java/lesson2_Classes_and_Objects/task1/MyArea.java
@@ -0,0 +1,9 @@
+package main.java.lesson2_Classes_and_Objects.task1;
+
+public class MyArea {
+    private static final double PI = 3.14;
+
+    static double areaOfCircle(int radius) {
+        return PI * Math.pow(radius, 2);
+    }
+}
---
+++ src/main/java/lesson2_Classes_and_Objects/task1/Program.java
@@ -0,0 +1,7 @@
+package main.java.lesson2_Classes_and_Objects.task1;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Площадь равна: " + MyArea.areaOfCircle(5));
+    }
+}
